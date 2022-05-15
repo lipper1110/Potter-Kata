4c4bff51170becd13813ebd2c63676d3cf4db11f
Willy Huang <s094398@gmail.com>
2022-05-15 23:08:17 +0800

    Edge testing

--- potter.py
+++ potter.py
@@ -1,18 +1,27 @@
-from collections import defaultdict
-def compute_total(book_count):
+def compute_total(books):
     discount = [1, 0.95, 0.9, 0.8, 0.75]
-    total = 0
-    for val in book_count.values():
-        total += 8 * val
-    total *= discount[len(book_count) - 1]
-    return total
-
+    books_dict = {0:0, 1:0, 2:0, 3:0, 4:0}
+    result = 0;
+    types = []
+    books_len = len(books)
+    for i in books:
+        books_dict[i] += 1
+    while books_len:
+        types = set([i for i in books_dict if books_dict[i]])
+        if books_len == 8 and len(types) == 5:
+            result += 6.4
+            books_len = 0
+            break
+        for i in range(5):
+            if books_dict[i]:
+                books_dict[i] -= 1
+                books_len -= 1
+        result += len(types) * discount[len(types) - 1 ]
+        types = []
+    return result * 8
 
 def price(books):
     if len(books) == 0:
         return 0
-    book_count = defaultdict(int)
-    for i in books:
-        book_count[i] += 1
-    return compute_total(book_count)
+    return compute_total(books)
 
--- test.py
+++ test.py
@@ -19,18 +19,18 @@ def testSimpleDiscounts():
     assert_equal(8 * 5 * 0.75, price([0, 1, 2, 3, 4]))
 
 
-# def testSeveralDiscounts():
-    # assert_equal(8 + (8 * 2 * 0.95), price([0, 0, 1]))
-    # assert_equal(2 * (8 * 2 * 0.95), price([0, 0, 1, 1]))
-    # assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), price([0, 0, 1, 2, 2, 3]))
-    # assert_equal(8 + (8 * 5 * 0.75), price([0, 1, 1, 2, 3, 4]))
+def testSeveralDiscounts():
+    assert_equal(8 + (8 * 2 * 0.95), price([0, 0, 1]))
+    assert_equal(2 * (8 * 2 * 0.95), price([0, 0, 1, 1]))
+    assert_equal((8 * 4 * 0.8) + (8 * 2 * 0.95), price([0, 0, 1, 2, 2, 3]))
+    assert_equal(8 + (8 * 5 * 0.75), price([0, 1, 1, 2, 3, 4]))
 
 
-# def testEdgeCases():
-    # assert_equal(2 * (8 * 4 * 0.8), price([0, 0, 1, 1, 2, 2, 3, 4]))
-    # assert_equal(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
-                 # price([0, 0, 0, 0, 0,
-                        # 1, 1, 1, 1, 1,
-                        # 2, 2, 2, 2,
-                        # 3, 3, 3, 3, 3,
-                        # 4, 4, 4, 4]))
+def testEdgeCases():
+    assert_equal(2 * (8 * 4 * 0.8), price([0, 0, 1, 1, 2, 2, 3, 4]))
+    assert_equal(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8),
+                 price([0, 0, 0, 0, 0,
+                        1, 1, 1, 1, 1,
+                        2, 2, 2, 2,
+                        3, 3, 3, 3, 3,
+                        4, 4, 4, 4]))
